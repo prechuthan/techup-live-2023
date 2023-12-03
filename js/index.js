d9b057d7e97c9601d5aaf4ae2fdaf8457deec063
Preshant Achuthan <engerium@gmail.com>
2023-12-03 23:35:28 +0800

    feat: Added filter for project categories

--- js/index.js
+++ js/index.js
@@ -1,16 +1,14 @@
 function convertProjectToHtmlCard(project) {
   const htmlCard = `
     <div class="col-xs-12 col-sm-6">
-        <article>
+        <article data-category="${project.category}">
             <img
             src="${project.screenshot}"
             alt="Screenshot of ${project.website}"
             />
             <strong>${project.name}</strong>
             <a href="${project.website}">${project.website}</a>
-            <p>
-            ${project.headline}
-            </p>
+            <p>${project.headline}</p>
         </article>
     </div>
     `;
@@ -22,4 +20,21 @@ $(async function () {
   const projects = await $.getJSON("../data/projects.json");
 
   await projects.map(convertProjectToHtmlCard);
+});
+
+$(function () {
+  $("#techup-categories").change(function () {
+    var selectedCategory = $(this).val();
+
+    $("#techup-projects article").each(function () {
+      var articleCategory = $(this).data("category");
+      var parentDiv = $(this).closest("div");
+
+      if (selectedCategory === "all" || articleCategory === selectedCategory) {
+        parentDiv.show();
+      } else {
+        parentDiv.hide();
+      }
+    });
+  });
 });
